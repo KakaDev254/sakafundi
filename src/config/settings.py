c2f1ad156901a35c4b48d39e51085c7d2f8993cf
Enock <oswereenock@gmail.com>
2026-09-08 00:57:08 +0300

    Updated SITE_ID for sakafundi.com

--- src/config/settings.py
+++ src/config/settings.py
@@ -277,7 +277,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-SITE_ID = 1
+SITE_ID = 2
 
 # ============================================================
 # ALLAUTH SETTINGS - UPDATED FOR EMAIL VERIFICATION
