fdcf6820120c231ee5c3985a912e0797aea4cd2e
Enock <oswereenock@gmail.com>
2026-09-07 17:05:17 +0300

    added zoho mail

--- src/core/context_processors.py
+++ src/core/context_processors.py
@@ -1,10 +1,14 @@
+# core/context_processors.py
 from django.conf import settings
 
 def site_settings(request):
     """Add site settings to all templates"""
     return {
-        'site_name': 'KenyaMarket',
+        'site_name': 'SakaFundi',  # Changed from 'KenyaMarket'
         'site_description': 'Kenya\'s leading service marketplace',
+        'site_email': 'info@sakafundi.com',  # Added this
+        'site_phone': '+254 700 123456',     # Added for consistency
+        'site_address': 'Nairobi, Kenya',    # Added for consistency
         'platform_fee': settings.PLATFORM_FEE_PERCENTAGE,
         'currency': settings.CURRENCY,
         'currency_symbol': settings.CURRENCY_SYMBOL,
